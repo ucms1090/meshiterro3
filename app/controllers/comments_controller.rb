4586189570941925aef67503a5a91d704965bf83
EC2 Default User <ec2-user@ip-172-31-34-69.ap-northeast-1.compute.internal>
2022-11-13 21:34:36 +0000

    save-18

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+
+  def craete
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.post_id = post.id
+    @comment.save
+    redirect_to posts_path
+  end
+
+  def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+
 end
