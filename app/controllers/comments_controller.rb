c9caf6182e8d2ec8158bbf3228e8ab31987b4f93
EC2 Default User <ec2-user@ip-172-31-34-69.ap-northeast-1.compute.internal>
2022-11-14 03:25:56 +0000

    save-18

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
 
-  def craete
+  def create
     @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
-    @comment.post_id = post.id
+    @comment.post_id = @post.id
     @comment.save
-    redirect_to posts_path
+    redirect_to post_path(@post.id)
   end
 
   def destroy
